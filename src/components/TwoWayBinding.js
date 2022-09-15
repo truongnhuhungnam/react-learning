8c80716ffeb444c74c73da6072b1d6acefa30a03
HUNGNAM\truon <truongnhuhungnam@gmail.com>
2022-09-15 20:35:10 +0700

    WIP

--- src/components/TwoWayBinding.js
+++ src/components/TwoWayBinding.js
@@ -37,7 +37,7 @@ const TwoWayBinding = () => {
     };
 
     const handleSubmitCheckbox = () => {
-        console.log({ checkboxChecked });
+        console.log([checkboxChecked]);
     };
 
     return (
